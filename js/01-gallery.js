9be59ee204853491a8439d941171641292e2ea37
YuriiPohorilets <pohorilets.y@gmail.com>
2022-08-12 13:49:42 +0300

    task #1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
+function makeGalleryItem({ preview, original, description }) {
+  return `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image"
+        src="${preview}" 
+        data-source="${original}"  
+        alt="${description}"
+        width="340">
+    </a>
+  </div>
+  `;
+}
+
+function onImgClick(e) {
+  e.preventDefault();
+
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="1280" alt="${e.target.alt}">`
+  );
+  instance.show();
+
+  function onEscKey(e) {
+    if (e.key === 'Escape') {
+      instance.close(instance);
+    }
+  }
+
+  window.addEventListener('keydown', onEscKey);
+}
+
+const makeGalleryList = galleryItems.map(makeGalleryItem).join('');
+
+refs.gallery.insertAdjacentHTML('afterbegin', makeGalleryList);
+
+refs.gallery.addEventListener('click', onImgClick);
